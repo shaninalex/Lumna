8758173992e1f4aef986d4c21d529ca57bb903cc
shaninalex <shanin.x@gmail.com>
2026-09-12 13:54:51 +0300

    feat(cli): workspace create and add member command

--- app/adapters/cli/identity.go
+++ app/adapters/cli/identity.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"text/tabwriter"
@@ -8,7 +9,9 @@ import (
 	"github.com/spf13/cobra"
 	"gitlab.com/shaninalex/lumna/app/core"
 	"gitlab.com/shaninalex/lumna/app/core/actor"
+	"gitlab.com/shaninalex/lumna/app/core/bus"
 	"gitlab.com/shaninalex/lumna/app/modules/identity/contract"
+	wContract "gitlab.com/shaninalex/lumna/app/modules/workspace/contract"
 )
 
 func NewIdentitiesRootCmd(app core.Resolve) *cobra.Command {
@@ -18,6 +21,58 @@ func NewIdentitiesRootCmd(app core.Resolve) *cobra.Command {
 	}
 
 	cmd.AddCommand(newIdentitiesListCmd(app))
+	cmd.AddCommand(newCreateIdentityCmd(app))
+
+	return cmd
+}
+
+var (
+	FailedToAddIdentityToWorkspaceError = errors.New("failed to add identity to workspace")
+)
+
+func newCreateIdentityCmd(app core.Resolve) *cobra.Command {
+	var email, fullName, password string
+	var active bool
+	var workspaceId int
+
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "Create identity",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			ctx := actor.With(cmd.Context(), actor.System())
+			profile, err := contract.ExecRegister(ctx, app(), contract.Register{
+				Email:    email,
+				FullName: fullName,
+				Password: bus.Secret(password),
+			})
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("Identity created: %s [%d]\n", profile.Email, profile.ID)
+
+			ok, err := wContract.ExecAddIdentityToWorkspace(ctx, app(), wContract.AddIdentityToWorkspace{
+				IdentityId:  profile.ID,
+				WorkspaceId: workspaceId,
+			})
+			if err != nil {
+				return err
+			}
+			if !ok {
+				return FailedToAddIdentityToWorkspaceError
+			}
+
+			fmt.Printf("Identity [%d] added to workspace [%d]\n", profile.ID, workspaceId)
+
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&email, "email", "", "Identity email")
+	cmd.Flags().StringVar(&fullName, "full-name", "", "Identity full name")
+	cmd.Flags().StringVar(&password, "password", "", "Identity raw password")
+	cmd.Flags().BoolVar(&active, "active", false, "Identity is active")
+	cmd.Flags().IntVar(&workspaceId, "workspace-id", 10, "Workspace Id belongs to")
 
 	return cmd
 }
