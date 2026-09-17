5a99f5f8159dd1e758e481e1550723a1c94cc9d3
shaninalex <shanin.x@gmail.com>
2026-09-17 06:54:49 +0300

    feat(board): load work items

--- app/adapters/http/handler/task/controller.go
+++ app/adapters/http/handler/task/controller.go
@@ -1,6 +1,8 @@
 package task
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"gitlab.com/shaninalex/lumna/app/adapters/http/transport"
 	"gitlab.com/shaninalex/lumna/app/core"
@@ -8,6 +10,7 @@ import (
 )
 
 func Register(resolve core.Resolve, router *gin.RouterGroup) {
+	router.GET("", handleList(resolve))
 	router.POST("", handleCreate(resolve))
 }
 
@@ -31,5 +34,28 @@ func handleCreate(resolve core.Resolve) gin.HandlerFunc {
 			return
 		}
 		transport.Success(c, toDTO(result))
+	}
+}
+
+func handleList(resolve core.Resolve) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Query("board_id"))
+		if err != nil {
+			transport.Fail(c, err)
+			return
+		}
+		result, err := contract.AskWorkItemList(c.Request.Context(), resolve(), contract.WorkItemList{
+			ScopeId: id,
+		})
+		if err != nil {
+			transport.Fail(c, err)
+			return
+		}
+
+		tasks := make([]taskDTO, len(result))
+		for i, item := range result {
+			tasks[i] = toDTO(item)
+		}
+		transport.Success(c, tasks)
 	}
 }
